baea24a50d0d073a33e5da29dc7ecb3c16f3d84e
sahithi-varma <sahitikanumuri@gmail.com>
2025-02-28 09:53:24 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+import streamlit as st
+import google.generativeai as genai
+
+API_KEY = "AIzaSyBsq5Kd5nJgx2fejR77NT8v5Lk3PK4gbH8"
+
+genai.configure(api_key=API_KEY)
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+if "chat" not in st.session_state:
+    st.session_state.chat = model.start_chat(history=[])
+
+st.title("🤖 Chatbot - How can i help you.")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Say something..."):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    response = st.session_state.chat.send_message(prompt)
+
+    st.session_state.messages.append({"role": "assistant", "content": response.text})
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
